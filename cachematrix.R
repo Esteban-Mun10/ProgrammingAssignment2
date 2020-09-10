a389493b769eec460c942f11a50a4ff867aca12e
Estebna-Mun10 <musesteban@gmail.com>
2020-09-10 13:48:57 -0500

    tarea de semana 3 se realizaran dos funciones
    La primera función, makeVector crea un "vector" especial, que en realidad es una lista que contiene una función para
    
    establecer el valor del vector
    obtener el valor del vector
    establecer el valor de la media
    obtener el valor de la media
    
    La siguiente función calcula la media del "vector" especial creado con la función anterior

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,37 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+####Esta primera función crea un objeto "matriz" especial que puede almacenar en caché su inverso.
 
+makeCacheMatrix <- function(x = matrix()) {
+    a <- NULL
+    set <- function(y){
+        x <<- y
+        a <<- NULL
+    }
+    get <- function()x
+    setInverse <- function(inverse) a <<- inverse
+    getInverse <- function() a 
+    list(set = set, get = get, 
+         setInverse = setInverse, 
+         getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
 
+### Esta segunda función calcula la inversa de la "matriz" especial creada por makeCacheMatrix arriba,
+### si ya se ha calculado la inversa (y la matriz no ha cambiado), entonces debería recuperar la inversa de la caché
+
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    a <- x$getInverse()
+    if(!is.null(a)){
+        message("getting cached data")
+        return(a)
+    }
+    mat <- x$get()
+    j <- solve(mat,...)
+    x$setInverse(a)
+    a
 }
